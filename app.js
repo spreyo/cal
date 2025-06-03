873fe7216da6548b13181e387301a28c6dad5057
spreyo <60149407+spreyo@users.noreply.github.com>
2025-06-03 13:23:41 +0200

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 app.use(helmet());
 
 app.use(express.json());
@@ -11,9 +12,17 @@ app.use(express.static(path.resolve("public")));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) =>{
-    res.sendFile(path.resolve("public", "webcal.ics"));
+app.get("/calendar", (req, res) =>{
+    const id = req.query.id;
+    const cal = fetch(`https://i6s5qfccjsaoxecm.public.blob.vercel-storage.com/calendars/${id}.ics`, {method: "GET", headers: {"Content-Type" : "text/calendar"}}).then(response =>{
+        response.text().then(text => {
+            res.setHeader("Content-Type", "text/calendar");
+            res.send(text);
+        });
+    })
 })
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
